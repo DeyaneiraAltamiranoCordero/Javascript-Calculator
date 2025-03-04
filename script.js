0386ed4c1c670231f3e6f6a66164cb245cb09907
DeyaneiraAltamiranoCordero <deyaneira.altamirano.cordero@est.una.ac.cr>
2025-03-04 11:28:31 -0600

    [FEAT] Body mass index calculator

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ document.getElementById('sumForm').addEventListener('submit', function(event) {
     let num2 = parseFloat(document.getElementById('num2').value) || 0;
     document.getElementById('resultado').textContent = num1 + num2;
 });
+
+document.getElementById("imcForm").addEventListener("input", () => {
+    const peso = parseFloat(document.getElementById("peso").value);
+    const altura = parseFloat(document.getElementById("altura").value);
+     const sistema = document.getElementById("sistema").value;
+
+    if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
+        document.getElementById("resultado").textContent = "";
+        return;
+    }
+
+    const imc = sistema === "metric" 
+        ? peso / Math.pow(altura / 100, 2) 
+        : (peso / Math.pow(altura, 2)) * 703;
+
+    document.getElementById("resultado").textContent = `Tu IMC es: ${imc.toFixed(2)}`;
+});
+
