4460c9c8825b4caff6251c27d9a3a62f9104e8cc
Jordan Ortiz <121458332+JordanOrtizMolina@users.noreply.github.com>
2025-03-03 20:28:15 -0600

    [FEAT] Add basic calculator functions

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-document.getElementById('sumForm').addEventListener('submit', function(event) {
+document.getElementById('calcForm').addEventListener('submit', function(event) {
     event.preventDefault();
+
     let num1 = parseFloat(document.getElementById('num1').value) || 0;
     let num2 = parseFloat(document.getElementById('num2').value) || 0;
-    document.getElementById('resultado').textContent = num1 + num2;
+    let operacion = document.getElementById('operacion').value;
+    let resultado;
+
+    switch (operacion) {
+        case 'sumar':
+            resultado = num1 + num2;
+            break;
+        case 'restar':
+            resultado = num1 - num2;
+            break;
+        case 'multiplicar':
+            resultado = num1 * num2;
+            break;
+        case 'dividir':
+            resultado = num2 !== 0 ? num1 / num2 : 'Error (División por 0)';
+            break;
+        default:
+            resultado = 'Operación no válida';
+    }
+
+    document.getElementById('resultado').textContent = resultado;
 });
